cafd4b65f8151250ce1c06ffc85253be1b281deb
Janderson Cassan <jande.max@gmail.com>
2017-09-04 15:47:46 -0300

    Commit inicial dos arquivos do projeto

---
+++ src/main/java/com/br/jande/pontoeletronico/PontoEletronicoApplication.java
@@ -0,0 +1,12 @@
+package com.br.jande.pontoeletronico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontoEletronicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontoEletronicoApplication.class, args);
+	}
+}
