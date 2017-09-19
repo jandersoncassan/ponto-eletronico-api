e8be977fdc7b934782317dc970ab71b7050c5ac1
Janderson Cassan <jande.max@gmail.com>
2017-09-19 17:21:25 -0300

    Add Empresa Controller

---
+++ src/main/java/com/br/jande/pontoeletronico/controller/EmpresaController.java
@@ -0,0 +1,64 @@
+package com.br.jande.pontoeletronico.controller;
+
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.br.jande.pontoeletronico.dtos.EmpresaDto;
+import com.br.jande.pontoeletronico.entities.Empresa;
+import com.br.jande.pontoeletronico.response.Response;
+import com.br.jande.pontoeletronico.service.EmpresaService;
+
+@RestController
+@RequestMapping("/api/empresas")
+@CrossOrigin("*")
+public class EmpresaController {
+
+	private static final Logger LOG = 	LoggerFactory.getLogger(EmpresaController.class);
+	
+	@Autowired
+	private EmpresaService empresaService;
+
+	/**
+	 * Retonar uma empresa de acordo com a empresa
+	 * 
+	 * @param cnpj
+	 * @return
+	 */
+	@GetMapping(value="/cnpj/{cnpj}")
+	public ResponseEntity<Response<EmpresaDto>> buscarPorCnpj(@PathVariable("cnpj") String cnpj){
+		LOG.info("Buscando empresa por CNPJ : {}", cnpj);
+		Response<EmpresaDto> response = new Response<EmpresaDto>();
+		Optional<Empresa> empresa = empresaService.buscarPorCnpj(cnpj);
+		
+		if(!empresa.isPresent()) {
+			LOG.info("Empresa não encontrada para o CNPJ : {}", cnpj);
+			response.getErrors().add("Empresa não encontrada para o CNPJ : "+ cnpj);
+			return ResponseEntity.badRequest().body(response);
+		}
+		response.setData(this.converterEmpresaDto(empresa.get()));
+		return ResponseEntity.ok(response);
+	}
+
+	/**
+	 * Converte a empresa DTO
+	 * 
+	 * @param empresa
+	 * @return
+	 */
+	private EmpresaDto converterEmpresaDto(Empresa empresa) {
+		EmpresaDto empresaDto = new EmpresaDto();
+		empresaDto.setId(empresa.getId());
+		empresaDto.setCnpj(empresa.getCnpj());
+		empresaDto.setRazaoSocial(empresa.getRazaoSocial());
+		return empresaDto;
+	}
+}
---
+++ src/main/java/com/br/jande/pontoeletronico/dtos/EmpresaDto.java
@@ -0,0 +1,47 @@
+package com.br.jande.pontoeletronico.dtos;
+
+public class EmpresaDto {
+
+	private Long id;
+	private String razaoSocial;
+	private String cnpj;
+	
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+	/**
+	 * @return the razaoSocial
+	 */
+	public String getRazaoSocial() {
+		return razaoSocial;
+	}
+	/**
+	 * @param razaoSocial the razaoSocial to set
+	 */
+	public void setRazaoSocial(String razaoSocial) {
+		this.razaoSocial = razaoSocial;
+	}
+	/**
+	 * @return the cnpj
+	 */
+	public String getCnpj() {
+		return cnpj;
+	}
+	/**
+	 * @param cnpj the cnpj to set
+	 */
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+	
+	
+}
